ec336dc772a47380ce71d62c059310318deed294
Mike Whit <2973130+mikegwhit@users.noreply.github.com>
2021-11-02 10:26:38 -0700

    Vue.js addition (#2)

--- scripts/start-express.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 80;
-
-app.use(express.static('www'));
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
